816c93e7942e752a0a87a6930cd27e7ae95b0e37
Nicolas Sauzede <nsauzede@laposte.net>
2015-03-20 09:58:50 +0100

    added spi

--- aaatop.vhd
+++ aaatop.vhd
@@ -52,6 +52,17 @@ signal interrupt : std_logic;
 signal interrupt_ack : std_logic;
 signal picoreset : std_logic;
 signal clk2 : std_logic;
+
+	signal spi_clk : std_logic;
+	signal spi_csn : std_logic;
+	signal spi_mosi : std_logic;
+	signal spi_miso : std_logic;
+	signal spimaster0_cs : std_logic;
+   SIGNAL wspi         : std_logic;
+	signal clk25 : std_logic;
+	signal cnt25 : std_logic_vector(2 downto 0);
+
+	signal LED : std_logic_vector(7 downto 0);
 begin
 	sump0 : entity work.BENCHY_sa_SumpBlaze_LogicAnalyzer8_jtag
 	port map(
@@ -61,7 +72,16 @@ begin
 		--armLED : out std_logic;
 		--triggerLED : out std_logic
 	);
-	la <= out_port;
+--	la <= LED;
+--	la <= cnt25(1 downto 0) & wspi & spimaster0_cs & spi_miso & spi_mosi & spi_csn & spi_clk;
+	la <= LED(3 downto 0) & spi_csn & spi_mosi & spi_miso & spi_clk;
+	process(clk2)
+	begin
+		if rising_edge(clk2) then
+			cnt25 <= cnt25 + 1;
+		end if;
+	end process;
+	clk25 <= cnt25(0);
 	kcpsm0: entity work.kcpsm3 Port map(
 		address => address,
 		instruction => instruction,
@@ -73,12 +93,40 @@ begin
 		interrupt => interrupt,
 		interrupt_ack => interrupt_ack,
 		reset => picoreset,
-		clk => clk2
+		clk => clk25
 	);
 	rom0 : entity work.rom Port map(
 		address => address,
 		instruction => instruction,
 		proc_reset => picoreset,
-		clk => clk2
+		clk => clk25
+	);
+	wspi <= write_strobe;
+	spimaster0_cs <= '1' when port_id(7 downto 3)="11110" else '0';
+	spimaster0 : entity work.spimaster
+	port map (
+		clk => clk25,
+		reset => picoreset,
+		cpu_address => port_id(2 downto 0),
+		cpu_wait => open,
+		data_in => out_port,
+		data_out => open,
+		enable => spimaster0_cs,
+		req_read => '0',
+		req_write => wspi,
+
+		slave_cs => spi_csn,
+		slave_clk => spi_clk,
+		slave_mosi => spi_mosi,
+		slave_miso => spi_miso
+	);
+	spislave0 : entity work.spi_vhdl
+	Port map( 
+		clk => clk25,
+		SCK => spi_clk,
+		MOSI => spi_mosi,
+		MISO => spi_miso,
+		SSEL => spi_csn,
+		LED => LED
 	);
 end Behavioral;
--- spi_slave.vhd
+++ spi_slave.vhd
@@ -1,108 +1,147 @@
 ----------------------------------------------------------------------------------
--- spi_slave.vhd
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:14:59 08/17/2011 
+-- Design Name: 
+-- Module Name:    spi_vhdl - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
 --
--- Copyright (C) 2006 Michael Poppitz
+-- Dependencies: 
 --
--- This program is free software; you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation; either version 2 of the License, or (at
--- your option) any later version.
---
--- This program is distributed in the hope that it will be useful, but
--- WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
--- General Public License for more details.
---
--- You should have received a copy of the GNU General Public License along
--- with this program; if not, write to the Free Software Foundation, Inc.,
--- 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
---
-----------------------------------------------------------------------------------
---
--- Details: http://www.sump.org/projects/analyzer/
---
--- spi_slave
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
 
-entity spi_slave is
-	port(
-		clock : in std_logic;
-		data : in std_logic_vector(31 downto 0);
-		send : in std_logic;
-		mosi : in std_logic;
-		sclk : in std_logic;
-		cs : in std_logic;
-		miso : out std_logic;
-		cmd : out std_logic_vector(39 downto 0);
-		execute : out std_logic;
-		busy : out std_logic;
-		dataReady : out std_logic;
-		reset : in std_logic;
-		tx_bytes : in integer range 0 to 4
-	);
-end spi_slave;
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
-architecture behavioral of spi_slave is
+entity spi_vhdl is
+    Port ( 
+	clk : in std_logic;
+	SCK : in std_logic;
+	MOSI : in std_logic;
+	MISO : out std_logic;
+	SSEL : in std_logic;
+	LED : out std_logic_vector(7 downto 0)
+	 );
+end spi_vhdl;
 
-	component spi_receiver
-		port(
-			rx : in std_logic;
-			clock : in std_logic;
-			sclk : in std_logic;
-			cmd : out std_logic_vector(39 downto 0);
-			execute : out std_logic;
-			reset : in std_logic;
-			cs : in std_logic
-		);
-	end component;
-
-	component spi_transmitter
-		port(
-			data : in std_logic_vector(31 downto 0);
-			tx_bytes : in integer range 0 to 4;
-			send : in std_logic;
-			clock : in std_logic;
-			sclk : in std_logic;
-			tx : out std_logic;
-			cs : in std_logic;
-			busy : out std_logic;
-			reset : in std_logic;
-			dataReady : out std_logic
-		);
-	end component;
-
+architecture Behavioral of spi_vhdl is
+signal SCKr : std_logic_vector(2 downto 0);
+signal SCK_risingedge : std_logic;
+signal SCK_fallingedge : std_logic;
+signal SSELr : std_logic_vector(2 downto 0);
+signal SSEL_active : std_logic;
+signal SSEL_startmessage : std_logic;
+signal SSEL_endmessage : std_logic;
+signal MOSIr : std_logic_vector(1 downto 0);
+signal MOSI_data : std_logic;
+signal bitcnt : unsigned(2 downto 0);
+signal byte_received : std_logic;
+signal byte_data_received : std_logic_vector(7 downto 0);
+signal RLED : std_logic_vector(7 downto 0);
+signal byte_data_sent : std_logic_vector(7 downto 0);
+signal cnt : unsigned(7 downto 0);
 begin
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			SCKr <= SCKr(1 downto 0) & SCK;
+		end if;
+	end process;
+	SCK_risingedge <= '1' when SCKr(2 downto 1) = "01" else '0';
+	SCK_fallingedge <= '1' when SCKr(2 downto 1) = "10" else '0';
 
-	Inst_spi_receiver: spi_receiver
-	port map(
-		rx => mosi,
-		clock => clock,
-		sclk => sclk,
-		cmd => cmd,
-		execute => execute,
-		reset => reset,
-		cs => cs
-	);
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			SSELr <= SSELr(1 downto 0) & SSEL;
+		end if;
+	end process;
+	SSEL_active <= not SSELr(1);
+	SSEL_startmessage <= '1' when SSELr(2 downto 1) = "10" else '0';
+	SSEL_endmessage <= '1' when SSELr(2 downto 1) = "01" else '0';
 
-	Inst_spi_transmitter: spi_transmitter
-	port map(
-		data => data,
-		tx_bytes => tx_bytes,
-		send => send,
-		clock => clock,
-		sclk => sclk,
-		tx => miso,
-		cs => cs,
-		busy => busy,
-		reset => reset,
-		dataReady => dataReady
-	);
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			MOSIr <= MOSIr(0) & MOSI;
+		end if;
+	end process;
+	MOSI_data <= MOSIr(1);
 
-end behavioral;
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if SSEL_active='0' then
+				bitcnt <= "000";
+			else
+				if SCK_risingedge='1' then
+					bitcnt <= bitcnt+1;
+					byte_data_received <= byte_data_received(6 downto 0) & MOSI_data;
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if SSEL_active='1' and SCK_risingedge='1' and bitcnt="111" then
+				byte_received <= '1';
+			else
+				byte_received <= '0';
+			end if;
+		end if;
+	end process;
+	
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if byte_received='1' then
+				RLED <= byte_data_received;
+			end if;
+		end if;
+	end process;
+	LED <= RLED;
 
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if SSEL_startmessage='1' then
+				cnt <= cnt+1;
+			end if;
+		end if;
+	end process;
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if SSEL_active='1' then
+				byte_data_sent <= std_logic_vector(cnt);
+			else
+				if SCK_fallingedge='1' then
+					if bitcnt="000" then
+						byte_data_sent <= x"00";
+					else
+						byte_data_sent <= byte_data_sent(6 downto 0) & '0';
+					end if;
+				end if;
+			end if;
+		end if;
+	end process;
+	MISO <= byte_data_sent(7);
+end Behavioral;
